e4b054083c4d2fcf17936dac5724f0329d67a92c
Scott Bender <scott@scottbender.net>
2018-09-01 11:07:42 -0400

    feature; add support for NOAA tides
    This also makes the calcs return a promise

--- calcs/worldtides.js
+++ calcs/worldtides.js
@@ -19,67 +19,68 @@ module.exports = function (app, plugin) {
     calculator: function (position) {
       app.debug('starting worldtides')
 
-      var now = Math.floor(new Date()/1000)
-      if(app.getSelfPath('environment.tide.timeHigh')){
-        heightHighTime = app.getSelfPath('environment.tide.timeHigh')
-      }
-      if(app.getSelfPath('environment.tide.timeLow')){
-        heightLowTime = app.getSelfPath('environment.tide.timeLow')
-      }
-      const endPoint = 'https://www.worldtides.info/api?extremes&lat='+position.latitude+'&lon='+position.longitude+'&length=52200&start='+now+'&datum=LAT&key='+plugin.properties.tides.worldtidesApiKey
-
-      if( typeof heightHighTime == 'undefined' || (now < heightHighTime || now < heightLowTime)){
-        agent('GET', endPoint).end().then(function onResult(response)  {
-          worldtidesToDeltas(JSON.parse(response.text))
-        })
-      }
-
-      // DEV TESTING
-      //var response = require('../responses/worldtides.json')
-      //worldtidesToDeltas(response)
-
-
-      function worldtidesToDeltas(response){
-        app.debug('updating tide')
-        if ( response.status != 200){
-          app.debug('worldtides response: ' + response.error?response.error:'none')
-        } else {
-          app.debug(JSON.stringify(response))
-          response.extremes.forEach((extreme, index) => {
-            if (index > 1) return
-            if (extreme.type == 'Low'){
-              heightLowTime = new Date(extreme.dt*1000).toISOString()
-              heightLow = extreme.height
-              updates.push({
-                path: 'environment.tide.heightLow',
-                value: heightLow,
-              })
-              updates.push({
-                path: 'environment.tide.timeLow',
-                value: heightLowTime
-              })
-            }
-            if (extreme.type == 'High'){
-              heightHighTime = new Date(extreme.dt*1000).toISOString()
-              heightHigh = extreme.height
-              updates.push({
-                path: 'environment.tide.heightHigh',
-                value: heightHigh
-              })
-              updates.push({
-                path: 'environment.tide.timeHigh',
-                value: heightHighTime
-              })
-            }
-          })
-          update = true
+      return new Promise((resolve, reject) => {
+        var now = Math.floor(new Date()/1000)
+        if(app.getSelfPath('environment.tide.timeHigh')){
+          heightHighTime = app.getSelfPath('environment.tide.timeHigh')
         }
-      }
-      if(update != true) {
-        return
-      } else {
-        return updates
-      }
+        if(app.getSelfPath('environment.tide.timeLow')){
+          heightLowTime = app.getSelfPath('environment.tide.timeLow')
+        }
+        const endPoint = 'https://www.worldtides.info/api?extremes&lat='+position.latitude+'&lon='+position.longitude+'&length=52200&start='+now+'&datum=LAT&key='+plugin.properties.tides.worldtidesApiKey
+
+        if( typeof heightHighTime == 'undefined' || (now < heightHighTime || now < heightLowTime)){
+          agent('GET', endPoint).end().then(function onResult(response)  {
+            worldtidesToDeltas(JSON.parse(response.text))
+          })
+        } else {
+          resolve(undefined)
+          return
+        }
+
+        // DEV TESTING
+        //var response = require('../responses/worldtides.json')
+        //worldtidesToDeltas(response)
+
+
+        function worldtidesToDeltas(response){
+          app.debug('updating tide')
+          if ( response.status != 200){
+            reject(new Error('worldtides response: ' + response.error?response.error:'none'))
+          } else {
+            app.debug(JSON.stringify(response))
+            let updates = []
+            response.extremes.forEach((extreme, index) => {
+              if (index > 1) return
+              if (extreme.type == 'Low'){
+                heightLowTime = new Date(extreme.dt*1000).toISOString()
+                heightLow = extreme.height
+                updates.push({
+                  path: 'environment.tide.heightLow',
+                  value: heightLow,
+                })
+                updates.push({
+                  path: 'environment.tide.timeLow',
+                  value: heightLowTime
+                })
+              }
+              if (extreme.type == 'High'){
+                heightHighTime = new Date(extreme.dt*1000).toISOString()
+                heightHigh = extreme.height
+                updates.push({
+                  path: 'environment.tide.heightHigh',
+                  value: heightHigh
+                })
+                updates.push({
+                  path: 'environment.tide.timeHigh',
+                  value: heightHighTime
+                })
+              }
+            })
+            resolve(updates)
+          }
+        }
+      })
     }
   }
 }
